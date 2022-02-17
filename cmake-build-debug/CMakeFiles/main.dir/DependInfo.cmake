1fa85ffb7a9a96228d78924b25951ff86f97d17d
Emily Wang <54878006+emily-l-wang@users.noreply.github.com>
2022-02-16 22:12:30 -0600

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/src/main.cxx" "C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/cmake-build-debug/CMakeFiles/main.dir/src/main.cxx.obj"
+  "C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/src/model.cxx" "C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/cmake-build-debug/CMakeFiles/main.dir/src/model.cxx.obj"
+  "C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/src/ui.cxx" "C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/cmake-build-debug/CMakeFiles/main.dir/src/ui.cxx.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src"
+  "../.cs211/lib/ge211/src/../include"
+  ".cs211/lib/ge211/src/../include"
+  "C:/MinGW/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/cmake-build-debug/.cs211/lib/ge211/src/CMakeFiles/ge211.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,10 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project
+# Build directory: C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test_model_test "model_test")
+set_tests_properties(Test_model_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/.cs211/cmake/211commands.cmake;90;add_test;C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/CMakeLists.txt;14;add_test_program;C:/Users/elw81/OneDrive/Desktop/2020-2021 Classes/Winter 2021/CS211/labs/final_project/CMakeLists.txt;0;")
+subdirs(".cs211/lib/catch")
+subdirs(".cs211/lib/ge211")
